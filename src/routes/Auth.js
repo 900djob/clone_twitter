076e5287ecbc0ad0d9235ef3055763a52dda09ee
seok <dreaming.seok@gmail.com>
2021-08-29 23:53:12 +0900

    :construction: auth3

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,51 +1,63 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [newAccount, setNewAccount] = useState(true);
   const onChange = (e) => {
-      const {
-          target: {name, value},
-      } = e;
-
-      if(name === email) {
-          setEmail(value);
-      } else if(name === password) {
-          setPassword(value);
-      }
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
   };
-
-
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
-
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
-          type="text"
+          name="email"
+          type="email"
           placeholder="Email"
           required
           value={email}
           onChange={onChange}
         />
         <input
-          type="text"
-          placeholder="password"
+          name="password"
+          type="password"
+          placeholder="Password"
           required
           value={password}
           onChange={onChange}
         />
-        <input type="button" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
-        <button>Continue with Googgle</button>
+        <button>Continue with Google</button>
         <button>Continue with Github</button>
       </div>
     </div>
   );
 };
-
 export default Auth;
