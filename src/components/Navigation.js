86cbf545ba88da0b6adcd34b6358fcad485acc75
seok <dreaming.seok@gmail.com>
2021-09-21 15:56:23 +0900

    :construction: update profile name

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navigation = () => (
-    <nav>
-        <ul>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li>
-                <Link to='/Profile'>My Prifile</Link>
-            </li>
-        </ul>
-    </nav>
-)
+const Navigation = ({ userObj }) => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Profile">{userObj.displayName}'s Prifile</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navigation;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,28 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { authService, dbService } from "../fbase";
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
 
-  const getMyNweets = async () => {
-    const nweets = await dbService
-      .collection("nweets")
-      .where("creatorId", "==", userObj.uid)
-      .orderBy("createdAt")
-      .get();
-      console.log(nweets.docs.map((doc) => doc.data()));
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
   };
 
-  useEffect(() => {
-    getMyNweets();
-  }, []);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+
+  // const getMyNweets = async () => {
+  //   const nweets = await dbService
+  //     .collection("nweets")
+  //     .where("creatorId", "==", userObj.uid)
+  //     .orderBy("createdAt")
+  //     .get();
+  //     console.log(nweets.docs.map((doc) => doc.data()));
+  // };
+
+  // useEffect(() => {
+  //   getMyNweets();
+  // }, []);
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type='text' placeholder='Display Name' value={newDisplayName} />
+        <input type='submit' value='Update Profile' />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
