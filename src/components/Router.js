d7bd64eb5d539b81703681ad29ad7bc647c7a33e
seok <dreaming.seok@gmail.com>
2021-09-21 16:29:48 +0900

    :construction: update profile bugfix

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,27 +2,26 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
-import Profile from "../routes/Profile"
+import Profile from "../routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({isLoggedIn, userObj}) => {
-
+const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation userObj={userObj}/>}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
           <>
-            <Route exact path='/'>
-              <Home userObj={userObj}/>
+            <Route exact path="/">
+              <Home userObj={userObj} />
             </Route>
-            <Route exact path='/Profile'>
-              <Profile userObj={userObj}/>
+            <Route exact path="/Profile">
+              <Profile userObj={userObj} refreshUser={refreshUser} />
             </Route>
           </>
         ) : (
           <>
-            <Route exact path='/'>
+            <Route exact path="/">
               <Auth />
             </Route>
           </>
